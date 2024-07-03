30dc6b506d05e6a10c65bce3d299f13a36701c8d
Hayama Umi <umi@umiblog.cn>
2024-07-03 17:02:38 +0800

    UPnP Auto Port Mapping

--- Tracker/Workers/UpnpDiscoverWorker.cs
+++ Tracker/Workers/UpnpDiscoverWorker.cs
@@ -1,7 +1,10 @@
+using System.Collections.Concurrent;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
+using System.Xml;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -13,7 +16,7 @@ using Umi.Dht.Client.UPnP;
 namespace Umi.Dht.Client.Workers;
 
 [Service(ServiceScope.Singleton)]
-public class UpnpDiscoverWorker(
+public partial class UpnpDiscoverWorker(
     ILogger<UpnpDiscoverWorker> logger,
     IOptions<KademliaConfig> kademliaConfig,
     IConfiguration configuration,
@@ -24,6 +27,16 @@ public class UpnpDiscoverWorker(
     private readonly Socket _socket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
     private readonly SocketAsyncEventArgs _socketAsyncEventArgs = new();
+
+    private readonly ConcurrentQueue<Func<Task>> _actions = new();
+
+    private const string GATEWAY_DEVICE = "InternetGatewayDevice:1";
+
+    private const string WAN_DEVICE = "WANDevice:1";
+
+    private const string WAN_CONNECTION_DEVICE = "WANConnectionDevice:1";
+
+    private const string WAN_IP_CONNECTION = "WANIPConnection:1";
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
@@ -39,13 +52,14 @@ public class UpnpDiscoverWorker(
         _socketAsyncEventArgs.SetBuffer(buffer);
         _socketAsyncEventArgs.Completed += this.OnReceivedData;
         _socketAsyncEventArgs.RemoteEndPoint = new IPEndPoint(local, 0);
+        _socketAsyncEventArgs.UserToken = new IPEndPoint(local, 0);
         if (!_socket.ReceiveFromAsync(_socketAsyncEventArgs))
         {
             this.OnReceivedData(_socket, _socketAsyncEventArgs);
         }
 
         const string discoverPackage =
-            "M-SEARCH * HTTP/1.1\r\nHOST: 239.255.255.250:1900\r\nMAN:\"ssdp:discover\"\r\nMX:3\r\nST:upnp:rootdevice\r\n";
+            "M-SEARCH * HTTP/1.1\r\nHOST: 239.255.255.250:1900\r\nMAN: \"ssdp:discover\"\r\nMX: 3\r\nST: upnp:rootdevice\r\n\r\n";
         logger.LogTrace("sending discover package {p}", discoverPackage);
         var sendByte = _socket.SendTo(Encoding.UTF8.GetBytes(discoverPackage), remoteGroup);
         logger.LogTrace("send bytes {s}", sendByte);
@@ -70,7 +84,7 @@ public class UpnpDiscoverWorker(
                     OperationalStatus: OperationalStatus.Up
                 }) continue;
             var ipProperties = nif.GetIPProperties();
-            
+
             var addresses = ipProperties.UnicastAddresses;
             foreach (var address in addresses)
             {
@@ -98,9 +112,10 @@ public class UpnpDiscoverWorker(
             return;
         }
 
+        logger.LogTrace("request remote is {remote}", args.RemoteEndPoint);
         var content = args.MemoryBuffer[..args.BytesTransferred];
         var result = Encoding.UTF8.GetString(content.Span);
-        logger.LogTrace("result is: {result}", result);
+        logger.LogTrace("result is: {result},", result);
         if (UPnPResponse.TryParse(result, out var response))
         {
             logger.LogTrace("upnp status code {code}", response.Code);
@@ -112,7 +127,7 @@ public class UpnpDiscoverWorker(
                 var values = response.Headers.GetValues("LOCATION");
                 if (values != null)
                 {
-                    this.TakeUPnPXml(values[0])
+                    this.TakeUPnPXml(values[0], args.RemoteEndPoint, ((IPEndPoint)args.UserToken!).Address)
                         .ConfigureAwait(false)
                         .GetAwaiter()
                         .OnCompleted(() => logger.LogTrace("action finished"));
@@ -121,6 +136,8 @@ public class UpnpDiscoverWorker(
         }
 
         // begin next
+        logger.LogTrace("next receive from {ip}", _socketAsyncEventArgs.UserToken);
+        _socketAsyncEventArgs.RemoteEndPoint = (IPEndPoint)_socketAsyncEventArgs.UserToken!;
         if (!_socket.ReceiveFromAsync(_socketAsyncEventArgs))
         {
             this.OnReceivedData(_socket, _socketAsyncEventArgs);
@@ -128,7 +145,7 @@ public class UpnpDiscoverWorker(
     }
 
 
-    private async Task TakeUPnPXml(string location)
+    private async Task TakeUPnPXml(string location, EndPoint? remote, IPAddress local)
     {
         logger.LogTrace("take xml from {location}", location);
         using var client = httpClientFactory.CreateClient();
@@ -140,9 +157,216 @@ public class UpnpDiscoverWorker(
         }
 
         var body = await response.Content.ReadAsStringAsync();
-        logger.LogTrace("server response body {body}", body);
-        
-        // TODO: UPnP next Operator, Check Added, and Add PortMapping
+        // var body = await File.ReadAllTextAsync(@"C:\Users\XY4\Desktop\igd.xml");
+        logger.LogTrace("remote {r},server response body {body}", remote, body);
+        XmlDocument document = new();
+        document.LoadXml(body);
+        // find root device and find operator node
+        // find gateway device
+        XmlNamespaceManager manager = new(document.NameTable);
+        var ns = document["root"]?.NamespaceURI ?? "urn:schemas-upnp-org:device-1-0";
+        manager.AddNamespace("gt", ns);
+        try
+        {
+            var gatewayNode = document.SelectSingleNode(
+                $"child::*/gt:device[contains(gt:deviceType, '{GATEWAY_DEVICE}')]",
+                manager);
+            if (gatewayNode == null)
+            {
+                logger.LogTrace("can not found {gateway} node", GATEWAY_DEVICE);
+                return;
+            }
+
+            var wanDevice = gatewayNode.SelectSingleNode(
+                $"child::*/gt:device[contains(gt:deviceType,'{WAN_DEVICE}')]",
+                manager);
+            if (wanDevice == null)
+            {
+                logger.LogError("can not found {wanDevice} node", WAN_DEVICE);
+                return;
+            }
+
+            var wanConnectionDevice = wanDevice.SelectSingleNode(
+                $"child::*/gt:device[contains(gt:deviceType, '{WAN_CONNECTION_DEVICE}')]",
+                manager);
+            if (wanConnectionDevice == null)
+            {
+                logger.LogError("can not found {wanConnectionDevice} node", WAN_CONNECTION_DEVICE);
+                return;
+            }
+
+            var wanIpConnection = wanConnectionDevice.SelectSingleNode(
+                $"child::*/gt:service[contains(gt:serviceType, '{WAN_IP_CONNECTION}')]",
+                manager);
+            if (wanIpConnection == null)
+            {
+                logger.LogError("can not found {wanIpConnection} node", WAN_IP_CONNECTION);
+                return;
+            }
+
+            var controlUrl = wanIpConnection["controlURL"]?.InnerText ?? "";
+            if (string.IsNullOrEmpty(controlUrl))
+            {
+                logger.LogError("can not found controlUrl, in ip connection {node}", wanIpConnection);
+                return;
+            }
+
+            if (!Uri.TryCreate(location, UriKind.Absolute, out var uri))
+            {
+                logger.LogError("error parse location {location}", location);
+                return;
+            }
+
+            var uriBuilder = new UriBuilder(uri.Scheme, uri.Host, uri.Port)
+            {
+                Path = controlUrl
+            };
+
+            logger.LogTrace("control url is {url}", uriBuilder.Uri);
+
+
+            await AddPortMapping(uriBuilder.Uri, wanIpConnection["serviceType"]!.InnerText, local);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "parsing node error");
+        }
+    }
+
+
+    private async Task AddPortMapping(Uri uri, string domain, IPAddress local)
+    {
+        try
+        {
+            logger.LogTrace("add port mapping, domain: {domain}", domain);
+            XmlDocument document = new();
+            var bodyNode = WarpSoapPackage(document);
+            // create add porting mapping request body
+            var addPortMappingBody = document.CreateElement("m", "AddPortMapping", domain);
+            bodyNode.AppendChild(addPortMappingBody);
+            // children
+            var nep = document.CreateElement("NewExternalPort");
+            nep.InnerText = kademliaConfig.Value.Port.ToString();
+            addPortMappingBody.AppendChild(nep);
+
+            var nip = document.CreateElement("NewInternalPort");
+            nip.InnerText = kademliaConfig.Value.Port.ToString();
+            addPortMappingBody.AppendChild(nip);
+            var protocol = document.CreateElement("NewProtocol");
+            protocol.InnerText = "UDP";
+            addPortMappingBody.AppendChild(protocol);
+            var enabled = document.CreateElement("NewEnabled");
+            enabled.InnerText = "1";
+            addPortMappingBody.AppendChild(enabled);
+            var nic = document.CreateElement("NewInternalClient");
+            nic.InnerText = local.ToString();
+            addPortMappingBody.AppendChild(nic);
+            var nld = document.CreateElement("NewLeaseDuration");
+            nld.InnerText = "0";
+            addPortMappingBody.AppendChild(nld);
+            var description = document.CreateElement("NewPortMappingDescription");
+            description.InnerText = "UmiKademliaTracker";
+            addPortMappingBody.AppendChild(description);
+            var nrh = document.CreateElement("NewRemoteHost");
+            addPortMappingBody.AppendChild(nrh);
+            var xml = await ConvertXmlString(document);
+            logger.LogTrace("send to body {xml}", xml);
+            var client = httpClientFactory.CreateClient();
+            using var postRequest = new HttpRequestMessage(HttpMethod.Post, uri);
+            using var requestContent = new StringContent(xml, Encoding.UTF8, "text/xml");
+            requestContent.Headers.Add("SOAPAction", $"{domain}#AddPortMapping");
+            postRequest.Content = requestContent;
+            postRequest.Headers.Add("Connection", "close");
+            postRequest.Headers.Add("Cache-Control", "no-cache");
+            postRequest.Headers.Add("Pragma", "no-cache");
+            using var response = await client.SendAsync(postRequest);
+            if (!response.IsSuccessStatusCode)
+            {
+                logger.LogWarning("error mapping port, status {s}", response.StatusCode);
+                return;
+            }
+
+            var result = await response.Content.ReadAsStringAsync();
+            logger.LogTrace("add port mapping result: {r}", result);
+            _actions.Enqueue(() => RemovePortMapping(uri, domain, local));
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "error add port mapping");
+        }
+    }
+
+
+    private static async Task<string> ConvertXmlString(XmlDocument document)
+    {
+        await using var ms = new MemoryStream();
+        await using var writer = XmlWriter.Create(new StreamWriter(ms, Encoding.UTF8), new XmlWriterSettings
+        {
+            Async = true,
+            Encoding = Encoding.UTF8,
+            OmitXmlDeclaration = false
+        });
+        document.Save(writer);
+        ms.Position = 0;
+        using var reader = new StreamReader(ms, Encoding.UTF8);
+        var xml = await reader.ReadToEndAsync();
+        return xml;
+    }
+
+    private static XmlElement WarpSoapPackage(XmlDocument document)
+    {
+        const string ns = "http://schemas.xmlsoap.org/soap/envelope/";
+        var envelope = document.CreateElement("s", "Envelope", ns);
+        var attribute = document.CreateAttribute("s", "encodingStyle", ns);
+        attribute.Value = "http://schemas.xmlsoap.org/soap/encoding/";
+        envelope.Attributes.Append(attribute);
+        document.AppendChild(envelope);
+        var body = document.CreateElement("s", "Body", ns);
+        envelope.AppendChild(body);
+        return body;
+    }
+
+    private async Task RemovePortMapping(Uri uri, string domain, IPAddress local)
+    {
+        logger.LogTrace("remove port mapping, domain: {domain}", domain);
+        try
+        {
+            XmlDocument document = new();
+            var body = WarpSoapPackage(document);
+            var deletePortMapping = document.CreateElement("m", "DeletePortMapping", domain);
+            body.AppendChild(deletePortMapping);
+            var nrh = document.CreateElement("NewRemoteHost");
+            deletePortMapping.AppendChild(nrh);
+            var nep = document.CreateElement("NewExternalPort");
+            nep.InnerText = kademliaConfig.Value.Port.ToString();
+            deletePortMapping.AppendChild(nep);
+            var protocol = document.CreateElement("NewProtocol");
+            protocol.InnerText = "UDP";
+            deletePortMapping.AppendChild(protocol);
+            var xml = await ConvertXmlString(document);
+            logger.LogTrace("send to body {xml}", xml);
+            var client = httpClientFactory.CreateClient();
+            using var postRequest = new HttpRequestMessage(HttpMethod.Post, uri);
+            using var requestContent = new StringContent(xml, Encoding.UTF8, "text/xml");
+            requestContent.Headers.Add("SOAPAction", $"{domain}#DeletePortMapping");
+            postRequest.Content = requestContent;
+            postRequest.Headers.Add("Connection", "close");
+            postRequest.Headers.Add("Cache-Control", "no-cache");
+            postRequest.Headers.Add("Pragma", "no-cache");
+            using var response = await client.SendAsync(postRequest);
+            if (!response.IsSuccessStatusCode)
+            {
+                logger.LogWarning("error mapping port, status {s}", response.StatusCode);
+                return;
+            }
+
+            var result = await response.Content.ReadAsStringAsync();
+            logger.LogTrace("delete port mapping result: {r}", result);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "remove port error");
+        }
     }
 
 
@@ -152,6 +376,18 @@ public class UpnpDiscoverWorker(
         _socket.Dispose();
         _socketAsyncEventArgs.Completed -= this.OnReceivedData;
         _socketAsyncEventArgs.Dispose();
+        logger.LogTrace("release all resource, when discard port mapping");
+        List<Task> tasks = [];
+        while (!_actions.IsEmpty)
+        {
+            if (_actions.TryDequeue(out var action))
+            {
+                tasks.Add(action());
+            }
+        }
+
+        Task.WaitAll(tasks.ToArray(), cancellationToken);
+
         return base.StopAsync(cancellationToken);
     }
 }
